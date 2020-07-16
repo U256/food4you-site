548714b753baf258f5014aa4263f63984c5e5e67
U256 <65955446+U256@users.noreply.github.com>
2020-07-16 17:45:29 +1000

    работа над блоком try-today

---
+++ js/slick-slider-init-file.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$('.carousel').slick({
+		centerMode: true,
+		centerPadding: '33%',
+		dots: true,
+
+		//autoplay: true,
+		prevArrow: '<button class="arrow prev"></button>',
+		nextArrow: '<button class="arrow next"></button>',
+		autoplaySpeed: 8000,
+		slidesToShow: 1,
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					centerMode: true,
+					centerPadding: '25%',
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					centerMode: true,
+					centerPadding: '15%',
+				}
+			}
+		]
+	});
+});
